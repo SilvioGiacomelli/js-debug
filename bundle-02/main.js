151dc311109025bd4b5bb10440e57b1fdb473ea5
SilvioGiacomelli <91.giacomelli.silvio@gmail.com>
2024-03-08 16:08:54 +0100

    Cartella 2 fatta

--- bundle-02/main.js
+++ bundle-02/main.js
@@ -10,7 +10,7 @@
 *******************************************************************************/
 
 
-// ESERCIZIO 1
+/*/ ESERCIZIO 1
 function checkAge() {
     const myAge = 32;
     const message = '';
@@ -22,16 +22,45 @@ function checkAge() {
     }
 }
 checkAge();
+*/// ESERCIZIO 1
+function checkAge() {
+    const myAge = 32;
+    let message = '';
 
-// ESERCIZIO 2
+    if (myAge < 18) {
+        message = `Sei troppo giovane! Hai ${myAge} anni!`;
+    } else {
+        message = 'Hai più di 18 anni!';
+    }
+}
+checkAge();
+
+/*
+Controllo maggiore età
+
+Non si può riassegnare il valore di una const
+*/
+
+/*/ ESERCIZIO 2
 function printColorsNumber() {
     const colors = ['blue', 'red', 'yellow', 'green', 'black'];
     console.log(`Nella mia palette ci sono ${colors.lenght} colori!`);
 }
 printColorsNumber();
+*/// ESERCIZIO 2
+function printColorsNumber() {
+    const colors = ['blue', 'red', 'yellow', 'green', 'black'];
+    console.log(`Nella mia palette ci sono ${colors.length} colori!`);
+}
+printColorsNumber();
 
+/*
+Identifico i colori della mia palette  nell'array
 
-// ESERCIZIO 3
+length scritto in maniera scorretta
+*/
+
+/*/ ESERCIZIO 3
 function addNumbers() {
     const userNumber = prompt('Inserisci un numero');
     const total = userNumber + 12;
@@ -39,9 +68,21 @@ function addNumbers() {
     console.log(`Il risultato finale è ${total}`);
 }
 addNumbers();
+*/// ESERCIZIO 3
+function addNumbers() {
+    const userNumber = prompt('Inserisci un numero');
+    const total = parseInt(userNumber) + 12;
 
+    console.log(`Il risultato finale è ${total}`);
+}
+addNumbers();
 
-// ESERCIZIO 4
+/*
+sommo il valore di prompt a 12
+userNumber è una stringa, non si può sommare a un valore numerico
+*/
+
+/*/ ESERCIZIO 4
 function checkAccess() {
     const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
     const userEmail = prompt('Inserisci il tuo indirizzo email');
@@ -59,9 +100,32 @@ function checkAccess() {
     }
 }
 checkAccess();
+*/// ESERCIZIO 4
+function checkAccess() {
+    const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
+    const userEmail = prompt('Inserisci il tuo indirizzo email');
 
+    let grantAccess = false;
 
-// ESERCIZIO 5 (suggerimento: c'è un solo errore)
+    if (addresses.includes(userEmail)) {
+        grantAccess = 'true';
+    }
+
+    if (grantAccess === true) {
+        console.log('Accesso consentito!');
+    } else {
+        console.log('Accesso negato!');
+    }
+}
+checkAccess();
+
+/*
+controllo correttezza email comparandola a quelle salvate nell'array con allert di avviso
+
+i valori booleani non vanno tra virgolette
+*/
+
+/*/ ESERCIZIO 5 (suggerimento: c'è un solo errore)
 function checkAccessImproved() {
     const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
 
@@ -88,8 +152,40 @@ function checkAccessImproved() {
         }
     }
     checkAccessImproved();
+*/// ESERCIZIO 5 (suggerimento: c'è un solo errore)
+function checkAccessImproved() {
+    const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
 
+    const userEmail = prompt('Inserisci il tuo indirizzo email');
 
+    let grantAccess = false;
+
+    for (let i = 0; i < addresses.length; i++) {
+        const email = addresses[i];
+
+        if (userEmail.length > 5) {
+
+            if (email === userEmail) {
+                grantAccess = true;
+
+            }
+
+        }
+
+        if (grantAccess) {
+            console.log('Accesso consentito!');
+        } else {
+            console.log('Accesso negato!');
+        }
+    }
+    checkAccessImproved(); 
+}
+/*
+Uguale sopra con controllo lunghezza mail
+
+i valori booleani non vanno tra virgolette
+Manca la graffa
+*/
 
 
 
